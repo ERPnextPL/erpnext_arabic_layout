73fe383bd688626de0278e0548e11ca8d46eff17
ERPtech-prog <biuro@erptech.pl>
2026-04-28 09:43:16 +0200

    Add ERPNext/Frappe v15 compatibility declaration

--- setup.py
+++ setup.py
@@ -15,5 +15,9 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=install_requires,
+	classifiers=[
+		"Framework :: Frappe",
+		"Framework :: Frappe :: 15",
+	],
 )
